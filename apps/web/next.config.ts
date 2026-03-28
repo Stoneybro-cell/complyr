9b4eab7b01461d7a94605a893090e43389c4da93
stoneybro <zionlivingstone4@gmail.com>
2026-03-28 06:26:06 +0100

    refactor: remove custom webpack configuration and flag from build script

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["fhevmjs"],
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-        config.resolve.fallback = {
-            ...config.resolve.fallback,
-            "tfhe_bg.wasm": false,
-        };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
